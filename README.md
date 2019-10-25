c08fb3302be45eb0da10b89fae5eeb1e49c4c404
Warllington Lima <55091486+Wjflima@users.noreply.github.com>
2019-10-24 21:40:06 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# TaskList00
-Created with CodeSandbox
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <h1>Lista de Tarefas Simples</h1>
+    <div>
+      <input v-model="novaTarefa" placeholder="Adicionar uma nova tarefa">
+      <button v-on:click="adicionar()">adicionar</button>
+    </div>
+    <div>
+      <ul>
+        <li v-for="({tarefa}, i) in lista" :key="tarefa" @click="remove(i)">{{tarefa}}</li>
+        <button v-on:click="remove()">Apagar</button>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      lista: [{}],
+      novaTarefa: ""
+    };
+  },
+  methods: {
+    adicionar() {
+      this.lista.push({
+        tarefa: this.novaTarefa
+      });
+      this.novaTarefa = "";
+    },
+    remove(i) {
+      this.lista.splice(i, 1);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
